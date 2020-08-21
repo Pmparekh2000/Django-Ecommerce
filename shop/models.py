343f13430ff702c71e003697a65bb87b40127b9a
Pmparekh2000 <pmp.tech2000@gmail.com>
2020-08-21 02:53:33 -0700

    Made Product model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    product_id = models.AutoField
+    # AutoField is auto incrementing field
+    product_name = models.CharField(max_length = 50)
+    product_desc = models.CharField(max_length = 300)
+    product_pub_date = models.DateField()
