d8ea0948d7d8f8d549348bdcae93a713c65ba7f1
Pmparekh2000 <pmp.tech2000@gmail.com>
2020-09-01 02:48:11 -0700

    Product Categories with multiple sliders

--- shop/views.py
+++ shop/views.py
@@ -6,11 +6,25 @@ from math import ceil
 # Create your tests here.
 
 def index(request):
-    products = Product.objects.all()
-    print(products)
-    n = len(products)
-    nSlides = n//4 + ceil((n/4) - (n//4))
-    params = {'nSlide': nSlides, 'range': range(1, nSlides), 'product': products}
+    # products = Product.objects.all()
+    # print(products.0.product_desc)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4) - (n//4))
+    # params = {'nSlide': nSlides, 'range': range(1, nSlides), 'product': products}
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
+    allProds = []
+    catProds = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catProds}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        # The above line forms a list of related categories
+        n = len(prod)
+        nSlides = n//4 + ceil((n/4) - (n//4))
+        # Below we are making list of lists.
+        # Each list has a particular category of product in it
+        allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds': allProds}
     return render(request, 'shop/index.html', params)
 
 def aboutus(request):
